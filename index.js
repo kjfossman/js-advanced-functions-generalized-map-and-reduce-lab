95d2c2fff0e3aa050035a02aad72ce3ae492ab4a
Kyle <kjfossman@gmail.com>
2021-05-17 16:02:47 -0800

    commit tree b5632b9808efa76746c3c4fb43505922a57c3094

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, callback){
+    const r = []
+    for(let i = 0; i < array.length; i++){
+        const element = array[i]
+        r.push(callback(element))
+    }
+    return r
+}
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    }
+  }
